7c786aa2f57216c7b12b27a29363671aca1682ee
Lucas Perez <lucastperez@gmail.com>
2024-02-16 02:30:01 -0300

    Adiciona LOCALTIMESTAMP para query GetCliente

--- models/clientes.go
+++ models/clientes.go
@@ -14,18 +14,19 @@ type Cliente struct {
 	Saldo  int
 }
 
-func GetCliente(db *sql.DB, id int) (*Cliente, error) {
-	row := db.QueryRow(`SELECT limite, saldo FROM clientes WHERE id = $1;`, id)
+func GetCliente(db *sql.DB, id int) (*Cliente, string, error) {
+	row := db.QueryRow(`SELECT limite, saldo, LOCALTIMESTAMP FROM clientes WHERE id = $1;`, id)
 
 	c := Cliente{ID: id}
+	var timeStamp string
 
-	err := row.Scan(&c.Limite, &c.Saldo)
+	err := row.Scan(&c.Limite, &c.Saldo, &timeStamp)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return nil, ErrNotFound
+			return nil, "", ErrNotFound
 		}
-		return nil, fmt.Errorf("get_cliente: %w", err)
+		return nil, "", fmt.Errorf("get_cliente: %w", err)
 	}
 
-	return &c, nil
+	return &c, timeStamp, nil
 }
