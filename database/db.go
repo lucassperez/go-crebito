85ff5ca3c44eb4ea8865fe344a611562291bc01a
Lucas Perez <lucastperez@gmail.com>
2024-02-15 22:16:10 -0300

    Cria pacote database para se conectar no banco

---
+++ database/db.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const pgString = "host=localhost port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
+
+func Database() (db *sql.DB, closeFunc func() error, err error) {
+	db, err = sql.Open("pgx", pgString)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("error at opening db: %w", err)
+	}
+	return db, db.Close, nil
+}
