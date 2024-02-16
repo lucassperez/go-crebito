eba9138d3f295b4dddd63e2aceb788931cc9f5b0
Lucas Perez <lucastperez@gmail.com>
2024-02-16 02:29:12 -0300

    Adiciona ORDER BY realizada_em DESC para script select-transacoes.sh

--- scripts/select-transacoes.sh
+++ scripts/select-transacoes.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker compose exec db psql -U postgres -d rinha-go-crebito --echo-all -c 'SELECT * FROM transacoes;'
+# ORDER BY (timestamp) DESC coloca as mais recentes primeiro
+docker compose exec db psql -U postgres -d rinha-go-crebito --echo-all -c 'SELECT * FROM transacoes ORDER BY realizada_em DESC;'
