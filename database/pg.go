065b6666fe47a1ad6a2237a676671d672562e3cc
Lucas Perez <lucastperez@gmail.com>
2024-02-18 01:45:49 -0300

    Refatora database/Database() para usar env var PG_HOST

--- database/pg.go
+++ database/pg.go
@@ -3,18 +3,39 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-const pgString = "host=localhost port=5432 user=postgres password=postgres dbname=rinha-go-crebito sslmode=disable"
-
 func Database() (db *sql.DB, closeFunc func() error, err error) {
-	db, err = sql.Open("pgx", pgString)
+	db, err = sql.Open("pgx", buildPgString())
 
 	if err != nil {
 		return nil, nil, fmt.Errorf("error at opening db: %w", err)
 	}
 
 	return db, db.Close, nil
+}
+
+func buildPgString() string {
+	host := getEnvOrDefault("PG_HOST", "localhost")
+
+	return fmt.Sprintf(
+		"host=%s "+
+			"port=5432 "+
+			"user=postgres "+
+			"password=postgres "+
+			"dbname=rinha-go-crebito "+
+			"sslmode=disable",
+		host,
+	)
+}
+
+func getEnvOrDefault(envName, defaultValue string) string {
+	v := os.Getenv(envName)
+	if v == "" {
+		return defaultValue
+	}
+	return v
 }
