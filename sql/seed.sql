5242ecf5c027177048e312e6971f6cf4a6e8a58a
Lucas Perez <lucastperez@gmail.com>
2024-02-18 22:38:04 -0300

    Remove inserção de transações no seed.sql

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO clientes (limite)
 VALUES (100000), (80000), (1000000), (10000000), (500000);
 
-INSERT INTO transacoes (valor, tipo, descricao, realizada_em, cliente_id)
-VALUES (1000, 'c', 'teste-cred', CURRENT_TIMESTAMP, 1);
+-- INSERT INTO transacoes (valor, tipo, descricao, realizada_em, cliente_id)
+-- VALUES (1000, 'c', 'teste-cred', CURRENT_TIMESTAMP, 1);
 
-INSERT INTO transacoes (valor, tipo, descricao, realizada_em, cliente_id)
-VALUES (50, 'd', 'teste-cred', CURRENT_TIMESTAMP, 1);
+-- INSERT INTO transacoes (valor, tipo, descricao, realizada_em, cliente_id)
+-- VALUES (50, 'd', 'teste-cred', CURRENT_TIMESTAMP, 1);
